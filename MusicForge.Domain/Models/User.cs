d897c622441739ff58779716a3c13b1339867c32
Jonas de Bruin <jonasdebruin@proton.me>
2026-05-04 00:03:57 +0200

    Make User values nullable

--- MusicForge.Domain/Models/User.cs
+++ MusicForge.Domain/Models/User.cs
@@ -16,10 +16,13 @@ namespace MusicForge.Domain.Models
 			Password = password;
 			Role = role;
 		}
-		public string FirstName { get; private set; }
-		public string LastName { get;  private set; }
-		public string Email { get;  private set; }
-		public string Password { get;  private set; }
-		public string Role { get;  private set; }
+
+		public User(){}
+
+		public string? FirstName { get; private set; }
+		public string? LastName { get;  private set; }
+		public string? Email { get;  private set; }
+		public string? Password { get;  private set; }
+		public string? Role { get;  private set; }
 	}
 }
