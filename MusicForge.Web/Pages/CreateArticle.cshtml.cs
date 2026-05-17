306c9f6ea57a7cd4716c3996df0e161583760cce
Jonas de Bruin <jonasdebruin@proton.me>
2026-05-18 00:21:06 +0200

    Display uploaded file with title on screen
    Just for testing

---
+++ MusicForge.Web/Pages/CreateArticle.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MusicForge.Domain.Models;
+
+namespace MusicForge.Web.Pages;
+
+[Authorize(Roles = UserRoles.User)]
+public class CreateArticle : PageModel
+{
+	[BindProperty]
+	public string Title { get; set; } = string.Empty;
+
+	[BindProperty]
+	public IFormFile? UploadedFile { get; set; }
+
+	public string? FileContents { get; set; }
+	public string? ErrorMessage { get; set; }
+
+	public void OnGet() { }
+
+	public async Task<IActionResult> OnPostAsync()
+	{
+		if (!ModelState.IsValid)
+			return Page();
+
+		if (UploadedFile == null || UploadedFile.Length == 0)
+		{
+			ErrorMessage = "Please select a file.";
+			return Page();
+		}
+
+		using var reader = new StreamReader(UploadedFile.OpenReadStream());
+		FileContents = await reader.ReadToEndAsync();
+
+		//TODO: Send to BLL and upload to db in DAL
+		return Page();
+	}
+}
