ace3f0eea75a16b409fc51c0861540b63bad1c7f
Jonas de Bruin <jonasdebruin@proton.me>
2026-05-18 10:42:13 +0200

    Add Id to User model

--- MusicForge.Domain/Models/User.cs
+++ MusicForge.Domain/Models/User.cs
@@ -3,6 +3,7 @@ namespace MusicForge.Domain.Models
 	public class User
 	{
 		public User(
+				Guid id,
 				string firstName,
 				string lastName,
 				string email,
@@ -10,6 +11,7 @@ namespace MusicForge.Domain.Models
 				string role
 				)
 		{
+			Id = id;
 			FirstName = firstName;
 			LastName = lastName;
 			Email = email;
@@ -17,12 +19,13 @@ namespace MusicForge.Domain.Models
 			Role = role;
 		}
 
-		public User(){}
+		public User() { }
 
+		public Guid Id { get; private set; }
 		public string? FirstName { get; private set; }
-		public string? LastName { get;  private set; }
-		public string? Email { get;  private set; }
-		public string? Password { get;  private set; }
-		public string? Role { get;  private set; }
+		public string? LastName { get; private set; }
+		public string? Email { get; private set; }
+		public string? Password { get; private set; }
+		public string? Role { get; private set; }
 	}
 }
