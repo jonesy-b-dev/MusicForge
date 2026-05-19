2808bf2b232badf0a847308c9b948d0ac661143c
Jonas de Bruin <jonasdebruin@proton.me>
2026-05-19 15:35:23 +0200

    Add test to get user

---
+++ MusicForge.Testing/UserServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using MusicForge.BLL.Services;
+using MusicForge.Domain.Interfaces;
+using MusicForge.Domain.Models;
+
+namespace MusicForge.Testing;
+
+public class UserServiceTests
+{
+	[Fact]
+	public void GetUserById_ReturnsCorrectUser()
+	{
+		// Arrange
+		var userGuid = Guid.NewGuid();
+		var expectedUser = new User(
+								userGuid,
+								"Bob",
+								"Bobbert",
+								"bob@bobbert.nl",
+								"asdf1234",
+								"User"
+								);
+
+		var mockRepo = new Mock<IUserRepository>();
+		mockRepo.Setup(r => r.GetUserById(userGuid))
+				.Returns(expectedUser);
+
+		var service = new UserService(mockRepo.Object);
+
+		// Act
+		var result = service.GetUserById(userGuid);
+
+		// Assert
+		Assert.NotNull(result);
+		Assert.Equal(userGuid, result.Id);
+		Assert.Equal("Bob", result.FirstName);
+		Assert.Equal("Bobbert", result.LastName);
+		Assert.Equal("bob@bobbert.nl", result.Email);
+		Assert.Equal("asdf1234", result.Password);
+		Assert.Equal("User", result.Role);
+	}
+
+}
