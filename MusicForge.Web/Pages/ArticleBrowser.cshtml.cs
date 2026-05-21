57f75fd8356ee13dc35760b9891afda4520e22e2
Jonas de Bruin <jonasdebruin@proton.me>
2026-05-21 13:30:17 +0200

    Added missing article browser page

---
+++ MusicForge.Web/Pages/ArticleBrowser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MusicForge.Web.Pages
+{
+    public class ArticleBrowserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
