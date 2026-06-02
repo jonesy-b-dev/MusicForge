37e5ce4233ee0746cbd37e496279209a0545c280
Jonas de Bruin <jonasdebruin@proton.me>
2026-06-03 00:17:45 +0200

    Article browser with simple mock data

--- MusicForge.Web/Pages/ArticleBrowser.cshtml.cs
+++ MusicForge.Web/Pages/ArticleBrowser.cshtml.cs
@@ -1,10 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MusicForge.Domain.Models;
 
 namespace MusicForge.Web.Pages
 {
     public class ArticleBrowserModel : PageModel
     {
+		public List<Article> mockArticles = [
+			new Article(Guid.NewGuid(),
+					Guid.NewGuid(),
+					"Article1",
+					"path/to/article",
+					-12,
+					DateTime.Now),
+			new Article(Guid.NewGuid(),
+					Guid.NewGuid(),
+					"Article2",
+					"path/to/article",
+					15,
+					DateTime.Now),
+			new Article(Guid.NewGuid(),
+					Guid.NewGuid(),
+					"Article3",
+					"path/to/article",
+					16,
+					DateTime.Now),
+			new Article(Guid.NewGuid(),
+					Guid.NewGuid(),
+					"Article4",
+					"path/to/article",
+					12,
+					DateTime.Now),
+		];
+
         public void OnGet()
         {
         }
