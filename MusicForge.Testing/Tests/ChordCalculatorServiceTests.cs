cdd5e0485feb5c1c6d7f233a90508e0e181aa28c
Jonas de Bruin <jonasdebruin@proton.me>
2026-06-18 13:15:37 +0200

    Add tests for chord/scale calculator

---
+++ MusicForge.Testing/Tests/ChordCalculatorServiceTests.cs
@@ -0,0 +1,21 @@
+using MusicForge.BLL.Services;
+using MusicForge.Domain.Models;
+
+namespace MusicForge.Testing.Tests
+{
+	public class ChordCalculatorServiceTests
+	{
+		// CalculateChord
+		[Fact]
+		public void CalculateChord_GetCMajorTriadChord()
+		{
+			// Arrange
+			NoteCollection expectedResult = new([Note.C, Note.E, Note.G]);
+			ChordCalculatorService _chordCalculatorService = new();
+
+			NoteCollection result = _chordCalculatorService.CalculateChord(Note.C, ChordMode.MajorTriad);
+
+			Assert.Equal(expectedResult.Notes, result.Notes);
+		}
+	}
+}
