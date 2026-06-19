2d331f5176ea74d48d31bbb032dab90d3a496bed
Jonas de Bruin <jonasdebruin@proton.me>
2026-06-19 11:38:12 +0200

    Display own articles on account page

--- MusicForge.Web/Pages/Account.cshtml.cs
+++ MusicForge.Web/Pages/Account.cshtml.cs
@@ -10,15 +10,23 @@ namespace MusicForge.Web.Pages;
 public class AccountModel : PageModel
 {
 	readonly UserService _userService;
+	readonly ArticleService _articleService;
+	public List<Article> _userArticles;
 	public User? loggedInUser;
-	public AccountModel(UserService userService)
+	public AccountModel(UserService userService, ArticleService articleService)
 	{
 		_userService = userService;
+		_articleService = articleService;
+
+		_userArticles = [];
 	}
 	public void OnGet()
 	{
 		Guid guid = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value
 				?? throw new InvalidOperationException("User identifier claim is missing on Account page load"));
 		loggedInUser = _userService.GetUserById(guid);
+
+
+		_userArticles = _articleService.GetAllArticlesFromWriter(guid);
 	}
 }
